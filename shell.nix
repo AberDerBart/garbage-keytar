8195d465620d0e52e1959fbdb46a384d275d3d81
Jonas Grosse-Holz <aber@der-b.art>
2021-09-05 14:30:08 +0200

    WIP: adapt shell.nix to nixos 21.05

--- shell.nix
+++ shell.nix
@@ -8,10 +8,6 @@ in pkgs.mkShell {
   buildInputs = [
     pkgs.cmake
     pkgs.gcc-arm-embedded
-    (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = with pkgs.vscode-extensions; [ ms-vscode.cpptools ];
-    })
-    pkgs.openocdpico
-    pkgs.openscad
+    # pkgs.openocdpico
   ];
 }

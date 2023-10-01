150d979edb46cb5881707bd20a42632d6e45c5e3
Jonas Grosse-Holz <aber@der-b.art>
2023-10-01 17:05:18 +0200

    fix arrow keys causing midi notes

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
@@ -23,28 +23,27 @@
  *
  */
 
-#include "tusb_config.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
 #include <string.h>
 
 #include "bsp/board.h"
-#include "tusb.h"
-#include "midi_note_table.h"
-#include "midi.h"
-#include "pitchbend.h"
+#include "class/hid/hid.h"
 #include "control.h"
 #include "display.h"
+#include "midi.h"
+#include "midi_note_table.h"
 #include "offset.h"
-#include "hid.h"
+#include "pico/stdlib.h"
+#include "pitchbend.h"
+#include "tusb.h"
+#include "tusb_config.h"
 
 extern void hid_task(void);
 
 void control_task();
 void pitchbend_task();
 
-int main(void)
-{
+int main(void) {
     board_init();
 
     tusb_init();
@@ -53,8 +52,7 @@ int main(void)
     control_init();
     display_init();
 
-    while (1)
-    {
+    while (1) {
         // tinyusb host task
         tuh_task();
 
@@ -68,14 +66,12 @@ int main(void)
     return 0;
 }
 
-void pitchbend_task()
-{
+void pitchbend_task() {
     uint8_t const channel = 0;
 
     static uint32_t start_ms = 0;
     const uint32_t interval_ms = 10;
-    if (board_millis() - start_ms < interval_ms)
-    {
+    if (board_millis() - start_ms < interval_ms) {
         return;
     }
     start_ms += interval_ms;
@@ -86,8 +82,8 @@ void pitchbend_task()
     };
     struct pitchbend_value pitchbend = pitchbend_read();
 
-    if(last_pitchbend.low != pitchbend.low || last_pitchbend.high != pitchbend.high){
-      midi_write(0xe0 | channel, pitchbend.low, pitchbend.high);
+    if (last_pitchbend.low != pitchbend.low || last_pitchbend.high != pitchbend.high) {
+        midi_write(0xe0 | channel, pitchbend.low, pitchbend.high);
     }
 
     last_pitchbend.low = pitchbend.low;
@@ -97,19 +93,18 @@ void pitchbend_task()
 void control_task() {
     static uint32_t start_ms = 0;
     const uint32_t interval_ms = 10;
-    if (board_millis() - start_ms < interval_ms)
-    {
+    if (board_millis() - start_ms < interval_ms) {
         return;
     }
     start_ms += interval_ms;
 
-    static uint8_t reported_values[2] = {0,0};
-    static uint8_t old_values[2] = {0,0};
+    static uint8_t reported_values[2] = {0, 0};
+    static uint8_t old_values[2] = {0, 0};
 
-    for(uint8_t i=0; i<2; i++){
+    for (uint8_t i = 0; i < 2; i++) {
         uint8_t val = read_control(i);
 
-        if(abs(val-reported_values[i]) > 1 || val != reported_values[i] && old_values[i] != reported_values[i]){
+        if (abs(val - reported_values[i]) > 1 || val != reported_values[i] && old_values[i] != reported_values[i]) {
             midi_write(0xb0, i, val);
             reported_values[i] = val;
         }
@@ -117,7 +112,6 @@ void control_task() {
         old_values[i] = val;
     }
 }
-    
 
 //--------------------------------------------------------------------+
 // USB HID
@@ -127,10 +121,8 @@ void control_task() {
 CFG_TUSB_MEM_SECTION static hid_keyboard_report_t usb_keyboard_report;
 
 // look up new key in previous keys
-static inline bool find_key_in_report(hid_keyboard_report_t const *p_report, uint8_t keycode)
-{
-    for (uint8_t i = 0; i < 6; i++)
-    {
+static inline bool find_key_in_report(hid_keyboard_report_t const *p_report, uint8_t keycode) {
+    for (uint8_t i = 0; i < 6; i++) {
         if (p_report->keycode[i] == keycode)
             return true;
     }
@@ -140,58 +132,67 @@ static inline bool find_key_in_report(hid_keyboard_report_t const *p_report, uin
 
 void clear_notes();
 
-void handle_key(uint8_t key, bool pressed)
-{
-    if(pressed){
-      if(key == 75){
-        clear_notes();
-        int16_t max_note = 67 + offset + 12;
-        if(max_note <= 127) {
-          offset += 12;
+void handle_key(uint8_t key, bool pressed) {
+#ifdef DEBUG_KEYS
+    char msg[16];
+    if (pressed) {
+        sprintf(msg, "KEY %d %d PRE", key, keycode2midi[key]);
+    } else {
+        sprintf(msg, "KEY %d %d REL", key, keycode2midi[key]);
+    }
+    debug(msg);
+    draw_info();
+#endif
+
+    if (pressed) {
+        if (key == 75) {
+            clear_notes();
+            int16_t max_note = 67 + offset + 12;
+            if (max_note <= 127) {
+                offset += 12;
+            }
+            draw_info();
+        } else if (key == 78) {
+            clear_notes();
+            int16_t min_note = 35 + offset - 12;
+            if (min_note > 0) {
+                offset -= 12;
+            }
+            draw_info();
+        } else if (key == 81) {
+            clear_notes();
+            int16_t min_note = 35 + offset - 1;
+            if (min_note > 0) {
+                offset--;
+            }
+            draw_info();
+        } else if (key == 82) {
+            clear_notes();
+            int16_t max_note = 67 + offset + 1;
+            if (max_note <= 127) {
+                offset++;
+            }
+            draw_info();
         }
-        draw_info();
-      } else if(key == 78){
-        clear_notes();
-        int16_t min_note = 35 + offset -12;
-        if(min_note > 0) {
-          offset -= 12;
-        }
-        draw_info();
-      } else if(key == 81){
-        clear_notes();
-        int16_t min_note = 35 + offset -1;
-        if(min_note > 0) {
-          offset--;
-        }
-        draw_info();
-      } else if(key == 82){
-        clear_notes();
-        int16_t max_note = 67 + offset +1;
-        if(max_note <= 127) {
-          offset++;
-        }
-        draw_info();
-      }
     }
 
-    uint8_t note = key < 128 ? keycode2midi[key] + offset : 0;
-    if (note != 0) {
+    uint8_t raw_note = key < 128 ? keycode2midi[key] : 0;
+    if (raw_note != 0) {
         board_led_write(pressed);
         if (pressed) {
-            midi_write(MIDI_NOTE_ON, note, 127);
+            midi_write(MIDI_NOTE_ON, raw_note + offset, 127);
         } else {
-            midi_write(MIDI_NOTE_ON, note, 0);
+            midi_write(MIDI_NOTE_ON, raw_note + offset, 0);
         }
     }
 }
 
 void clear_notes() {
-  midi_write(MIDI_CHANNEL_MODE, MIDI_CHANNEL_MODE_ALL_SOUND_OFF_B1, MIDI_CHANNEL_MODE_ALL_SOUND_OFF_B2);
+    midi_write(MIDI_CHANNEL_MODE, MIDI_CHANNEL_MODE_ALL_SOUND_OFF_B1, MIDI_CHANNEL_MODE_ALL_SOUND_OFF_B2);
 }
 
-static inline void process_kbd_report(hid_keyboard_report_t const *p_new_report)
-{
-    static hid_keyboard_report_t prev_report = {0, 0, {0}}; // previous report to check key released
+static inline void process_kbd_report(hid_keyboard_report_t const *p_new_report) {
+    static hid_keyboard_report_t prev_report = {0, 0, {0}};  // previous report to check key released
 
     // check key presses
     for (uint8_t i = 0; i < 6; i++) {
@@ -212,36 +213,30 @@ static inline void process_kbd_report(hid_keyboard_report_t const *p_new_report)
     prev_report = *p_new_report;
 }
 
-void tuh_hid_keyboard_mounted_cb(uint8_t dev_addr)
-{
+void tuh_hid_keyboard_mounted_cb(uint8_t dev_addr) {
     // application set-up
     printf("A Keyboard device (address %d) is mounted\r\n", dev_addr);
 
     tuh_hid_keyboard_get_report(dev_addr, &usb_keyboard_report);
 }
 
-void tuh_hid_keyboard_unmounted_cb(uint8_t dev_addr)
-{
+void tuh_hid_keyboard_unmounted_cb(uint8_t dev_addr) {
     // application tear-down
     printf("A Keyboard device (address %d) is unmounted\r\n", dev_addr);
 }
 
 // invoked ISR context
-void tuh_hid_keyboard_isr(uint8_t dev_addr, xfer_result_t event)
-{
+void tuh_hid_keyboard_isr(uint8_t dev_addr, xfer_result_t event) {
     (void)dev_addr;
     (void)event;
 }
 
 #endif
 
-void hid_task(void)
-{
+void hid_task(void) {
     uint8_t const addr = 1;
-    if (tuh_hid_keyboard_is_mounted(addr))
-    {
-        if (!tuh_hid_keyboard_is_busy(addr))
-        {
+    if (tuh_hid_keyboard_is_mounted(addr)) {
+        if (!tuh_hid_keyboard_is_busy(addr)) {
             process_kbd_report(&usb_keyboard_report);
             tuh_hid_keyboard_get_report(addr, &usb_keyboard_report);
         }
--- midi.c
+++ midi.c
@@ -1,22 +1,30 @@
-#include <stdint.h>
-#include "hardware/uart.h"
-#include "hardware/gpio.h"
 #include "midi.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include "display.h"
+#include "hardware/gpio.h"
+#include "hardware/uart.h"
 
 #define UART_ID uart0
 #define BAUD_RATE 31250
 #define MIDI_OUT_PIN 0
 #define MIDI_IN_PIN 1
 
-void midi_init()
-{
+void midi_init() {
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(MIDI_OUT_PIN, GPIO_FUNC_UART);
     gpio_set_function(MIDI_IN_PIN, GPIO_FUNC_UART);
 }
 
-void midi_write(uint8_t cmd, uint8_t b1, uint8_t b2)
-{
+void midi_write(uint8_t cmd, uint8_t b1, uint8_t b2) {
+#ifdef DEBUG_MIDI
+    char msg[16];
+    sprintf(msg, "MIDI %02X %02X %02X", cmd, b1, b2);
+    debug(msg);
+    draw_info();
+#endif
     uart_putc(UART_ID, cmd);
     uart_putc(UART_ID, b1);
     uart_putc(UART_ID, b2);

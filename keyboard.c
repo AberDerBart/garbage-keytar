7a6beae424065d7a92d4f198f365dee38a612448
Jonas Grosse-Holz <aber@der-b.art>
2023-10-03 19:48:54 +0200

    migrate to pio usb

--- keyboard.c
+++ keyboard.c
@@ -7,6 +7,35 @@
 #include "display.h"
 #include "settings.h"
 #include "midi_note_table.h"
+#include "main_host.h"
+
+void process_kbd_report(hid_keyboard_report_t const *p_new_report);
+
+void keyboard_init() {
+
+}
+
+void keyboard_task() {
+  keyboard_event_message msg;
+
+  while(queue_try_remove(&keyboard_event_queue, &msg)){
+    switch(msg.type) {
+    case KEYBOARD_CONNECTED:
+      display_debug("KEY CONN");
+      break;
+    case KEYBOARD_DISCONNECTED:
+      display_debug("KEY DC");
+      break;
+    case KEYBOARD_REPORT:
+      display_debug("REPORT");
+      process_kbd_report(&msg.report);
+      break;
+    default:
+      display_debug("UNKNOWN");
+      break;
+    }
+  }
+}
 
 void handle_key(uint8_t key, bool pressed) {
 #ifdef DEBUG_KEYS
@@ -16,54 +45,78 @@ void handle_key(uint8_t key, bool pressed) {
     } else {
         sprintf(msg, "KEY %d %d REL", key, keycode2midi[key]);
     }
-    debug(msg);
-    draw_info();
+    display_debug(msg);
 #endif
 
     if (pressed) {
         if (key >= 58 && key <= 69) {
             program = key - 58;
             midi_write2(MIDI_PROGRAM_CHANGE, program);
-            draw_info();
         }
         if (key == 75) {
             int16_t max_note = 67 + offset + 12;
             if (max_note <= 127) {
                 clear_notes();
                 offset += 12;
-                draw_info();
             }
         } else if (key == 78) {
             int16_t min_note = 35 + offset - 12;
             if (min_note > 0) {
                 clear_notes();
                 offset -= 12;
-                draw_info();
             }
         } else if (key == 81) {
             int16_t min_note = 35 + offset - 1;
             if (min_note > 0) {
                 clear_notes();
                 offset--;
-                draw_info();
             }
         } else if (key == 82) {
             int16_t max_note = 67 + offset + 1;
             if (max_note <= 127) {
                 clear_notes();
                 offset++;
-                draw_info();
             }
         }
     }
 
     uint8_t raw_note = key < 128 ? keycode2midi[key] : 0;
     if (raw_note != 0) {
-        board_led_write(pressed);
         if (pressed) {
             midi_write3(MIDI_NOTE_ON, raw_note + offset, 127);
         } else {
             midi_write3(MIDI_NOTE_ON, raw_note + offset, 0);
         }
     }
+}
+
+static inline bool find_key_in_report(hid_keyboard_report_t const *p_report, uint8_t keycode) {
+    for (uint8_t i = 0; i < 6; i++) {
+        if (p_report->keycode[i] == keycode)
+            return true;
+    }
+
+    return false;
+}
+
+void process_kbd_report(hid_keyboard_report_t const *p_new_report) {
+    static hid_keyboard_report_t prev_report = {0, 0, {0}};  // previous report to check key released
+
+    // check key presses
+    for (uint8_t i = 0; i < 6; i++) {
+        uint8_t key = p_new_report->keycode[i];
+        if (key && !find_key_in_report(&prev_report, key)) {
+            handle_key(key, true);
+        }
+    }
+
+    // check key releases
+    for (uint8_t i = 0; i < 6; i++) {
+        uint8_t key = prev_report.keycode[i];
+        if (key && !find_key_in_report(p_new_report, key)) {
+            handle_key(key, false);
+        }
+    }
+
+    prev_report = *p_new_report;
 }

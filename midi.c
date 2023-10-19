d902ca772098898fa74a322d863725ebafbc32c1
Jonas Grosse-Holz <aber@der-b.art>
2023-10-19 16:37:25 +0200

    send bluetooth messages along with midi messages

--- midi.c
+++ midi.c
@@ -6,6 +6,7 @@
 #include "display.h"
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
+#include "ble_midi.h"
 
 #define UART_ID uart0
 #define BAUD_RATE 31250
@@ -19,24 +20,32 @@ void midi_init() {
 }
 
 void midi_write3(uint8_t cmd, uint8_t b1, uint8_t b2) {
-#ifdef DEBUG_MIDI
-    char msg[16];
-    sprintf(msg, "MIDI %02X %02X %02X", cmd, b1, b2);
-    display_debug(msg);
-#endif
     uart_putc(UART_ID, cmd);
     uart_putc(UART_ID, b1);
     uart_putc(UART_ID, b2);
+
+    uint8_t msg[3] = { cmd, b1, b2 };
+    ble_midi_send_msg(3, msg);
+
+  #ifdef DEBUG_MIDI
+    char display_msg[16];
+    sprintf(display_msg, "MIDI %02X %02X %02X", cmd, b1, b2);
+    display_debug(display_msg);
+  #endif
 }
 
 void midi_write2(uint8_t cmd, uint8_t b1) {
-#ifdef DEBUG_MIDI
-    char msg[16];
-    sprintf(msg, "MIDI %02X %02X", cmd, b1);
-    display_debug(msg);
-#endif
     uart_putc(UART_ID, cmd);
     uart_putc(UART_ID, b1);
+
+    uint8_t msg[2] = { cmd, b1 };
+    ble_midi_send_msg(2, msg);
+
+  #ifdef DEBUG_MIDI
+    char display_msg[16];
+    sprintf(display_msg, "MIDI %02X %02X", cmd, b1);
+    display_debug(display_msg);
+  #endif
 }
 
 void clear_notes() {

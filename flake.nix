82822692dcbbe5af2c870c04e85586124c585040
Jonas Grosse-Holz <aber@der-b.art>
2024-01-04 13:03:03 +0100

    add gdb make targets

--- flake.nix
+++ flake.nix
@@ -2,17 +2,13 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      #picoprobe_overlay = _: prev: {
-      #  openocd = prev.callPackage ./openocd/with_picoprobe.nix {};
-      #};
-      #
       pkgs = import nixpkgs { inherit system; };
     in {
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = [
           pkgs.cmake
           pkgs.gcc-arm-embedded
-          pkgs.openocd
+          pkgs.openocd-rp2040
           pkgs.minicom
         ];
         

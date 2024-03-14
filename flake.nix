d4de4607616d905000e19bb36ac2b4073ac6e72b
Jonas Grosse-Holz <aber@der-b.art>
2024-03-14 11:41:19 +0100

    firmware: build bin2c tool using nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,21 @@
     let
       pkgs = import nixpkgs { inherit system; };
     in {
+      packages.bin2c = pkgs.stdenv.mkDerivation {
+        name = "bin2c";
+        src = pkgs.fetchFromGitHub {
+          owner = "daschr";
+          repo = "pico-ssd1306";
+          rev = "8467f5b06ebd7bfa7b53e4ebc83dfdc6f396e4eb";
+          sha256 = "sha256-dwd67nhH9SRAbbMBIecUXf2lKCT6Yxc8hdn0qxqdp/8=";
+        };
+        #buildPhase = "make -C $src/firmware/pico-ssd1306/tools";
+        buildPhase = "make -C tools";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp tools/bin2c $out/bin/bin2c
+        '';
+      };
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = [
           pkgs.cmake
@@ -11,6 +26,7 @@
           pkgs.openocd-rp2040
           pkgs.minicom
           pkgs.imagemagick
+          self.packages.${system}.bin2c
         ];
         
         PICO_SDK_PATH = "./firmware/pico-sdk";

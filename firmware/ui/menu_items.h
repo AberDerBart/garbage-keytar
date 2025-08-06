e0700eb19a9ccafad0995155c82edb4451f1b94d
Jonas Grosse-Holz <aber@der-b.art>
2025-08-06 17:07:25 +0200

    firmware: add ui_menu_item_uint8_t

--- firmware/ui/menu_items.h
+++ firmware/ui/menu_items.h
@@ -11,3 +11,6 @@ ui_pos_t ui_menu_item_default_render(ui_element_t* item, ssd1306_t* display,
                                      ui_pos_t pos);
 
 ui_element_t* make_default_menu_item(char* label, void (*action)());
+
+ui_element_t* ui_menu_item_uint8_new(char* label, uint8_t* value, uint8_t min,
+                                     uint8_t max, void (*on_change)());

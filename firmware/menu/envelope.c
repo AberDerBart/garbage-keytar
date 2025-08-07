7dbf1d2fa19ed2d6349ea4e76d851892876c4ce6
Jonas Grosse-Holz <aber@der-b.art>
2025-08-07 20:46:52 +0200

    firmware: extract menu_add_envelope_entries function

--- firmware/menu/envelope.c
+++ firmware/menu/envelope.c
@@ -4,17 +4,21 @@
 #include "ui/menu.h"
 #include "ui/menu_items.h"
 
-void push_menu_envelope() {
-  ui_menu_t* menu = push_menu(4);
-  menu_add(menu, ui_menu_item_uint8_new("Attack", &adsr_settings.attack, 0, 127,
-                                        NULL));
+void menu_add_envelope_entries(ui_menu_t* menu, adsr_settings_t* envelope) {
+  menu_add(menu,
+           ui_menu_item_uint8_new("Attack", &envelope->attack, 0, 127, NULL));
 
   menu_add(menu,
-           ui_menu_item_uint8_new("Decay", &adsr_settings.decay, 0, 127, NULL));
+           ui_menu_item_uint8_new("Decay", &envelope->decay, 0, 127, NULL));
 
-  menu_add(menu, ui_menu_item_uint8_new("Sustain", &adsr_settings.sustain, 0,
-                                        127, NULL));
+  menu_add(menu,
+           ui_menu_item_uint8_new("Sustain", &envelope->sustain, 0, 127, NULL));
 
-  menu_add(menu, ui_menu_item_uint8_new("Release", &adsr_settings.release, 0,
-                                        127, NULL));
+  menu_add(menu,
+           ui_menu_item_uint8_new("Release", &envelope->release, 0, 127, NULL));
+}
+
+void push_menu_envelope() {
+  ui_menu_t* menu = push_menu(4);
+  menu_add_envelope_entries(menu, &adsr_settings);
 }
--- firmware/menu/envelope.h
+++ firmware/menu/envelope.h
@@ -1,3 +1,7 @@
 #pragma once
+#include "adsr.h"
+#include "ui/menu.h"
+
+void menu_add_envelope_entries(ui_menu_t* menu, adsr_settings_t* envelope);
 
 void push_menu_envelope();
